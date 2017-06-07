5faed60b7bcfefedb85d853640ee3e60b7cab3fd
hss01248 <hss012489@gmail.com>
2017-06-07 22:27:15 +0800

    shiro config

--- spmvc/src/main/java/com/hss01248/spmvc/controller/LoginController.java
+++ spmvc/src/main/java/com/hss01248/spmvc/controller/LoginController.java
@@ -1,11 +1,18 @@
 package com.hss01248.spmvc.controller;
 
 import com.hss01248.spmvc.bean.BaseNetBean;
+import com.hss01248.spmvc.exception.CustomException;
 import com.hss01248.spmvc.service.impl.UserServiceImpl;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -38,6 +45,38 @@ public class LoginController {
 
 
         return userService.regist(phone,pw,httpServletRequest);
+
+    }
+
+
+    /**
+     * shiro管理的登录
+     * 此方法不处理认证成功,而是会自动跳到上一个页面(可配置)
+     * @param model
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping("/login")
+    public String loginsubmit(Model model, HttpServletRequest request)
+            throws Exception {
+
+        // shiro在认证过程中出现错误后将异常类路径通过request返回
+        String exceptionClassName = (String) request
+                .getAttribute("shiroLoginFailure");
+        if(exceptionClassName!=null){
+            if (UnknownAccountException.class.getName().equals(exceptionClassName)) {
+                throw new CustomException("账号不存在");
+            } else if (IncorrectCredentialsException.class.getName().equals(
+                    exceptionClassName)) {
+                throw new CustomException("用户名/密码错误");
+            } else if("randomCodeError".equals(exceptionClassName)){
+                throw new CustomException("验证码错误");
+            } else{
+                throw new Exception();//最终在异常处理器生成未知错误
+            }
+        }
+        return "login";
 
     }
 
