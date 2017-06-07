75b4c0681ebcaabe76bdcb064d8874211013d788
hss01248 <hss012489@gmail.com>
2017-06-07 21:30:04 +0800

    mapper

---
+++ batisdemo/src/main/java/com/hss01248/spmvc/auth/mapper/SysUserRoleMapper.java
@@ -0,0 +1,30 @@
+package com.hss01248.spmvc.auth.mapper;
+
+import com.hss01248.spmvc.auth.po.SysUserRole;
+import com.hss01248.spmvc.auth.po.SysUserRoleExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface SysUserRoleMapper {
+    long countByExample(SysUserRoleExample example);
+
+    int deleteByExample(SysUserRoleExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(SysUserRole record);
+
+    int insertSelective(SysUserRole record);
+
+    List<SysUserRole> selectByExample(SysUserRoleExample example);
+
+    SysUserRole selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") SysUserRole record, @Param("example") SysUserRoleExample example);
+
+    int updateByExample(@Param("record") SysUserRole record, @Param("example") SysUserRoleExample example);
+
+    int updateByPrimaryKeySelective(SysUserRole record);
+
+    int updateByPrimaryKey(SysUserRole record);
+}
---
+++ spmvc/src/main/java/com/hss01248/spmvc/auth/mapper/SysUserRoleMapper.java
@@ -0,0 +1,31 @@
+package com.hss01248.spmvc.auth.mapper;
+
+import com.hss01248.spmvc.auth.po.SysUserRole;
+import com.hss01248.spmvc.auth.po.SysUserRoleExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface SysUserRoleMapper {
+    long countByExample(SysUserRoleExample example);
+
+    int deleteByExample(SysUserRoleExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(SysUserRole record);
+
+    int insertSelective(SysUserRole record);
+
+    List<SysUserRole> selectByExample(SysUserRoleExample example);
+
+    SysUserRole selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") SysUserRole record, @Param("example") SysUserRoleExample example);
+
+    int updateByExample(@Param("record") SysUserRole record, @Param("example") SysUserRoleExample example);
+
+    int updateByPrimaryKeySelective(SysUserRole record);
+
+    int updateByPrimaryKey(SysUserRole record);
+}
